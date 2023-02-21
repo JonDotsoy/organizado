36c0e8234c005f176d426b224f7b4388d08c52ff
Jonathan Delgado <hi@jon.soy>
2023-02-21 06:27:43 -0300

    feat: upgrade installer

--- install.sh
+++ install.sh
@@ -1,8 +1,42 @@
 #!/usr/bin/env sh
 
+deno --version &>/dev/null
+DENO_EXISTS=$?
+git --version &>/dev/null
+GIT_EXISTS=$?
+
+if [ $DENO_EXISTS != 0 ]; then
+  echo "Deno is required" >&2
+fi
+
+if [ $GIT_EXISTS != 0 ]; then
+  echo "Git is required" >&2
+fi
+
+if [[ $DENO_EXISTS != 0 || $GIT_EXISTS != 0 ]]; then
+  exit 1
+fi
+
+SOURCE_DIR=$(
+  if [[ -z $SOURCE_DIR ]]; then
+    echo $HOME/.organizado/repo/organizado
+  else
+    echo $SOURCE_DIR
+  fi
+)
+
+if [ -d $SOURCE_DIR/.git/ ]; then
+  (
+    cd $SOURCE_DIR
+    git pull -f
+  )
+else
+  git clone https://github.com/JonDotsoy/organizado $SOURCE_DIR
+fi
+
 deno install -f \
   --allow-env=HOME \
   --allow-run=git \
   --allow-read="$HOME/.organizado/" \
   --allow-write="$HOME/.organizado/" \
-  $(dirname $0)/cli.ts
+  $SOURCE_DIR/cli.ts
